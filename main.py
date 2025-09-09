517ecbdaa91c80002b35e3cf678b3818a71a9e2e
Andres <anacleto115@hotmail.com>
2025-09-08 21:02:57 -0500

    Cambios de la 9:02 08-09-2025

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import datetime;
+import decimal;
+
+class Estados:
+    id: int = 0;
+    nombre: str = None;
+
+"""
+    int (int-long)
+    float, decimal, complex
+    str
+    datetime (datetime.datetime.now())
+    bool (True, False)
+"""
+
+estado = Estados();
 print("Hola mundo!");
