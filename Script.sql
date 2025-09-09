9666ea87baf4b54b5a7e6c04dd115c1ccf344a19
Andres <anacleto115@hotmail.com>
2025-09-08 20:37:05 -0500

    Cambios de la 8:37 08-09-2025

--- Script.sql
+++ Script.sql
@@ -1,2 +1,19 @@
 
 CREATE DATABASE db_universidad;
+
+CREATE TABLE `db_universidad`.`estados` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+    `nombre` VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `db_universidad`.`personas` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+    `cedula` VARCHAR(50) NOT NULL UNIQUE,
+    `nombre` VARCHAR(250) NOT NULL,
+	`estado` INT NOT NULL,
+	`fecha` DATETIME NOT NULL,
+	`activo` BIT NOT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_personas__estados` FOREIGN KEY (`estado`) REFERENCES `estados`(`id`)
+);
